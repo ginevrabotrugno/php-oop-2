a5bfa02dc6b375a319c3ebcd7c03ebba7d255295
ginevrabotrugno <ginevrabotrugno.gb@gmail.com>
2024-09-04 17:24:04 +0200

    feat: add static markup and style

--- data/db.php
+++ data/db.php
@@ -21,5 +21,6 @@ $products = [
     new Toy("Pallina da gioco", "Pallina resistente per cani", 5.99, $dogCategory, "Rubber"),
     new Bed("Cuccia Morbida", "Cuccia comoda e morbida per gatti e cani", 35.99, $genericCategory, "Large"),
     new Toy("Giochino per Gatti", "Giochino con piume per gatti", 7.99, $catCategory, "Feather"),
-    new Bed("Cuccia Comoda per Cani", "Cuccia comoda per cani", 40.00, $dogCategory, "Medium")
+    new Bed("Cuccia Comoda per Cani", "Cuccia comoda per cani", 40.00, $dogCategory, "Medium"),
+    new Food("Cibo per Gatti Gourmet", "Cibo gourmet per gatti con ingredienti pregiati", 30.00, $catCategory, "Gourmet")
 ];
--- index.php
+++ index.php
@@ -3,9 +3,6 @@
 require_once __DIR__ . '/Model/Product.php';
 require_once __DIR__ . '/data/db.php';
 
-var_dump($products);
-// var_dump($categories);
-
 ?>
 
 <!DOCTYPE html>
@@ -13,9 +10,98 @@ var_dump($products);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
     <title>OOP 2</title>
 </head>
 <body>
+
+    <h1 class="green_txt">Our Products</h1>
+    
+    <div class="container">
+        <div class="row">
+        <div class="card">
+                <h3 class="card_title">Nome Prodotto</h3>
+                <p class="prod_description">Descrizione: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae illo voluptate similique deserunt! Aut eveniet consequuntur molestias cum tenetur accusantium modi adipisci nulla nihil, minima fuga dignissimos voluptate libero harum.</p>
+                <span class="price">Prezzo</span>
+                <h4 class="list_title">Categoria</h4>
+                <ul>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                </ul>
+                <span>Adatto a...</span>
+            </div>
+            <div class="card">
+                <h3 class="card_title">Nome Prodotto</h3>
+                <p class="prod_description">Descrizione: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae illo voluptate similique deserunt! Aut eveniet consequuntur molestias cum tenetur accusantium modi adipisci nulla nihil, minima fuga dignissimos voluptate libero harum.</p>
+                <span class="price">Prezzo</span>
+                <h4 class="list_title">Categoria</h4>
+                <ul>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                </ul>
+                <span>Adatto a...</span>
+            </div>
+            <div class="card">
+                <h3 class="card_title">Nome Prodotto</h3>
+                <p class="prod_description">Descrizione: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae illo voluptate similique deserunt! Aut eveniet consequuntur molestias cum tenetur accusantium modi adipisci nulla nihil, minima fuga dignissimos voluptate libero harum.</p>
+                <span class="price">Prezzo</span>
+                <h4 class="list_title">Categoria</h4>
+                <ul>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                </ul>
+                <span>Adatto a...</span>
+            </div>
+            <div class="card">
+                <h3 class="card_title">Nome Prodotto</h3>
+                <p class="prod_description">Descrizione: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae illo voluptate similique deserunt! Aut eveniet consequuntur molestias cum tenetur accusantium modi adipisci nulla nihil, minima fuga dignissimos voluptate libero harum.</p>
+                <span class="price">Prezzo</span>
+                <h4 class="list_title">Categoria</h4>
+                <ul>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                </ul>
+                <span>Adatto a...</span>
+            </div>
+            <div class="card">
+                <h3 class="card_title">Nome Prodotto</h3>
+                <p class="prod_description">Descrizione: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae illo voluptate similique deserunt! Aut eveniet consequuntur molestias cum tenetur accusantium modi adipisci nulla nihil, minima fuga dignissimos voluptate libero harum.</p>
+                <span class="price">Prezzo</span>
+                <h4 class="list_title">Categoria</h4>
+                <ul>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                </ul>
+                <span>Adatto a...</span>
+            </div>
+            <div class="card">
+                <h3 class="card_title">Nome Prodotto</h3>
+                <p class="prod_description">Descrizione: Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae illo voluptate similique deserunt! Aut eveniet consequuntur molestias cum tenetur accusantium modi adipisci nulla nihil, minima fuga dignissimos voluptate libero harum.</p>
+                <span class="price">Prezzo</span>
+                <h4 class="list_title">Categoria</h4>
+                <ul>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                    <li>List Item</li>
+                </ul>
+                <span>Adatto a...</span>
+            </div>
+        </div>
+    </div>
     
 </body>
 </html>
